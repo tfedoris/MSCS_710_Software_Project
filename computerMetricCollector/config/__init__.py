849d5e28b859da8aa4a5ee431ea31866854c7928
timhoangt <timothy.hoang1@marist.edu>
2021-04-17 19:37:07 -0400

    Update __init__.py

--- computerMetricCollector/config/__init__.py
+++ computerMetricCollector/config/__init__.py
@@ -2,7 +2,7 @@ import json
 
 
 def import_config(root_path):
-    full_file_path = root_path + "\\config\\setting.json"
+    full_file_path = root_path + "/config/setting.json" # "\\config\\setting.json"
     with open(file=full_file_path) as f:
         settings = json.load(f)
     return settings
