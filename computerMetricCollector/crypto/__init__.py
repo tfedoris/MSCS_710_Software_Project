735527e42ef6d232b28f1d9ae66d4078363b50c3
Yi Lin <Yi.Lin2@marist.edu>
2021-05-15 18:51:04 -0400

    Push bug fix for getting public key

--- computerMetricCollector/crypto/__init__.py
+++ computerMetricCollector/crypto/__init__.py
@@ -16,14 +16,16 @@ def get_key(logger, reg_id, url):
     logger.debug("registration_id: " + str(reg_id))
     logger.debug("URL: " + str(url))
     attempt = 0
+    fail = True
     public_key = None
-    while attempt < 2:
+    while attempt < 2 and fail:
         response = requests.post(url, json={"registration_id": reg_id})
         res_json = response.json()
         logger.debug("Sucesss: " + str(res_json.get("success")))
         if res_json.get("success") and res_json.get("data"):
             data = res_json.get("data")
             public_key = data.get("public_key")
+            fail = False
         else:
             print("Fail to get public key using registration id: " + reg_id)
             print("Attempt to fetch public key again.")
